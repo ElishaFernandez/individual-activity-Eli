a69021e264d2db3d085b0d1d2037cfd8ac45efc8
ElishaFernandez <fernandezelisha19@gmail.com>
2026-01-09 08:45:45 +0800

    ee

--- game.js
+++ game.js
@@ -3,17 +3,52 @@ class Hero {
         this.name = n;
         this.health = 100;
     }
+    
 }
 
+setWeapon(weapon); {
+    this.weapon = weapons[weapon];
+    this.weaponName = weapon;
+    messageLog(`${this.name} has equipped a ${this.weaponName}.`);
+
+
 const weapons = {
-  Sword:(Damage=20, Accuracy=0.8),
-  Bow:(Damage=15, Accuracy=0.9),
-  Axe:(Damage=25, Accuracy=0.7)
+    Sword:(Damage=20, Accuracy=0.8),
+    Bow:(Damage=15, Accuracy=0.9),
+    Axe:(Damage=25, Accuracy=0.7)
 };
+
+attack(opponent) {
+        log(`${this.name} attacks ${opponent.name} 
+                 with a ${this.weaponName} for ${damage} damage!`);
+            opponent.takeDamage(damage);
+        // if (Math.random() < this.weapon.hitChance) {
+        //     const damage = Math.floor(Math.random() * 
+        //     (this.weapon.maxDamage - this.weapon.minDamage + 1)) 
+        //     + this.weapon.minDamage;
+        //     log(`${this.name} attacks ${opponent.name} 
+        //         with a ${this.weaponName} for ${damage} damage!`);
+        //     opponent.takeDamage(damage);
+        // } else {
+        //     log(`${this.name} missed the attack with their 
+        //         ${this.weaponName}!`);
+        // }
+    }
+    
+     let attacker = playerNum === 1 ? player1 : player2;
+    let defender = playerNum === 1 ? player2 : player1;
+    let selectedWeapon = document.getElementById
+    (`player${playerNum}-weapon`).value;
+    attacker.setWeapon(selectedWeapon);
+    log(attacker.name + " using " + attacker.selectedWeapon 
+        + " as attacker !! " + defender.name + " defender");
+    attacker.attack(defender);
+   
+
 
 let player1, player2;
 let currentplayer;
-
+ 
 function startGame() {
     player1 = new Hero(document.getElementById("player1-name").value.trim());
     player2 = new Hero(document.getElementById("player2-name").value.trim());
@@ -31,3 +66,18 @@ function messageLog(msg) {
     gamelog.scrollTop = gamelog.scrollHeight;
 }
 
+function selectWeapon(player, weapon, imgElem) {
+    var inputId = 'player' + player + '-weapon';
+    var hidden = document.getElementById(inputId);
+    if (!hidden) return;
+    hidden.value = weapon;
+    // highlight selection
+    var container = document.getElementById('player' + player + '-weapon-choices');
+    if (!container) return;
+    var imgs = container.getElementsByClassName('weapon-img');
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].classList.remove('weapon-selected');
+    }
+    imgElem.classList.add('weapon-selected');
+    messageLog(`${player === 1 ? player1.name : player2.name} selected ${weapon} as his/her weapon.`);
+}
